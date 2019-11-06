9c89d03a5bd288ad668235b822ecc2c5aa45e38e
ezziagheith <ezziagheith@gmail.com>
2019-11-06 15:58:59 -0800

    Created Player Form

--- pick_up_game_app/views.py
+++ pick_up_game_app/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 
-from .models import Event
-from .forms import EventForm
+from .models import Event, Player
+from .forms import EventForm, PlayerForm
 
 # Create your views here.
 def home(request):
@@ -23,3 +23,20 @@ def event_create(request):
         form = EventForm()
     context = {'form':form, 'header': "Add New Event"}
     return render(request, 'event_form.html', context)
+
+
+
+def player_create(request):
+    if request.method == 'POST':
+        form = PlayerForm(request.POST)
+        if form.is_valid():
+            player = form.save(commit=False)
+            player.user = request.user
+            artist.save()
+            return redirect('profile.html', pk=player.pk)
+    else:
+        form = PlayerForm()
+    context = {'form': form, 'header': "Add New Player"}
+    return render(request, 'player_form.html', context)
+
+            
