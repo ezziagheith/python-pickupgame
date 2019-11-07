391d848dbb21cdaf49c1d56104e89b22f1b32a87
adilkhan <adilkhan@ucsb.edu>
2019-11-07 14:57:05 -0800

    saving changes

--- pick_up_game_app/views.py
+++ pick_up_game_app/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from django.core import serializers
 from django.contrib.auth.decorators import login_required
 
-from .models import Event, Player, Event_Player, Event_User
+from .models import Event, Player, Event_Player
 from .forms import EventForm, PlayerForm
 
 # Create your views here.
@@ -18,13 +18,7 @@ def event_create(request):
             event = form.save(commit=False)
             event.user = request.user
             event.save()
-            event_user = Event_User.objects.create(
-                user=request.user,
-                event=event
-            )
-            event_user.save()
             return redirect('event_detail', pk=event.pk)
-            
     else:
         form = EventForm()
     context = {'form':form, 'header': "Add New Event"}
@@ -38,11 +32,12 @@ def player_create(request):
         if form.is_valid():
             player = form.save(commit=False)
             player.user = request.user
-            player.save()
+            artist.save()
             return redirect('profile.html', pk=player.pk)
     else:
         form = PlayerForm()
     context = {'form': form, 'header': "Add New Player"}
     return render(request, 'player_form.html', context)
 
-
+def event_listAll(request):
+    return render(request, 'event_list.html', context)
