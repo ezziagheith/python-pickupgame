4e41240fc71eff7702478dcb0159cf92906abb7f
ezziagheith <ezziagheith@gmail.com>
2019-11-08 10:12:22 -0800

    Delete route

--- pick_up_game_app/urls.py
+++ pick_up_game_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('addplayer/', views.player_create, name="player_create"),
     path('events/<int:pk>', views.event_detail, name="event_detail"),
     path('events/edit_event/<int:pk>', views.event_edit, name='event_edit'),
+    path('events/<int:pk>/delete/', views.event_delete, name= 'event_delete')
     # path('about/', views.aboutus, name ='about'),
     
 
