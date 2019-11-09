46b703e03909dc695aecb72aa64368c0660089c8
adilkhan <adilkhan@ucsb.edu>
2019-11-08 16:38:43 -0800

    commit: added some more CSS to the project

--- pick_up_game_app/urls.py
+++ pick_up_game_app/urls.py
@@ -5,10 +5,10 @@ urlpatterns = [
     path('', views.landing, name="landing"),
     path('addevent/', views.event_create, name="event_create"),
     path('addplayer/', views.player_create, name="player_create"),
-    path('events/', views.event_listAll, name="event_list")
+    path('events/', views.event_listAll, name="event_list"),
     path('events/<int:pk>', views.event_detail, name="event_detail"),
     path('events/edit_event/<int:pk>', views.event_edit, name='event_edit'),
-    path('events/<int:pk>/delete/', views.event_delete, name= 'event_delete')
+    path('events/<int:pk>/delete/', views.event_delete, name= 'event_delete'),
     # path('about/', views.aboutus, name ='about'),
     
 
